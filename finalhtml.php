6c1a667d418dc2f3d80040980c52d4b0bdec107f
cmward7400 <43648498+cmward7400@users.noreply.github.com>
2018-11-29 20:02:59 -0500

    Create finalhtml.php

---
+++ finalhtml.php
@@ -0,0 +1,87 @@
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+ <meta name = "viewport"
+
+  content = "width = device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="finalcss.css"/>
+
+</head>
+
+<body>
+
+	<div class = "nav">
+
+	  <ul>
+	
+	    <li><a href="#home">Home</a></li>
+
+		<li><a href = "#shoppingcart">Shopping Cart</a></li>
+
+		<li><a href = "login.php">Log in</a></li>
+
+		<li><a href = "register.php">Create Account</a></li>
+
+		<li><a href = "logout.php">Logout</a></li>
+	
+	  </ul>
+
+	</div>
+
+
+ <!---Background image src: https://www.mandarin-gear.com/products/mandarin-gear-copper-stainless-steel-french-press-coffee-maker-28-oz-800-ml
+  --->
+  
+  
+	<div class = "header1">
+
+	  <h1><i>Handcrafted Tea and Coffee</i></h1>
+
+	</div>
+
+	
+
+	<div class = "row">
+
+	   <p><b>Sign-in or create an account to purchase one-of-a-kind, 
+
+	   handcrafted coffee or tea!</p>
+
+	   
+
+	   <div class = "header2">
+
+	     <h1>Coffee</h1>
+
+	   </div>
+
+		 	
+
+	   <p> dfjgh jsk h skjdh kasdjh gdkfgjh sdkfhg</p>
+
+			
+
+	   <div class = "header3">
+
+	     <h1>Tea</h1>
+
+	   </div>
+
+	   
+
+	   <p>dsjgfasjdhkjasdkglhaslkdjgh hagkjas laehrgka aldhf;kglh</p>
+
+	
+
+	</div>
+
+	
+
+</body>
+
+</html>
