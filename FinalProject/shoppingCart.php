3488bcf3dab6f0cefc581ec4e39b874e2a36065b
cmward7400 <43648498+cmward7400@users.noreply.github.com>
2018-12-04 11:54:10 -0500

    Update shoppingCart.php

--- FinalProject/shoppingCart.php
+++ FinalProject/shoppingCart.php
@@ -1,94 +1,250 @@
 <!DOCTYPE HTML>
+
 <html>  
+
 <head>
+
+
 
  <meta name = "viewport"
 
+
+
   content = "width = device-width, initial-scale=1.0">
+
+
 
   <link rel="stylesheet" href="finalcss.css"/>
 
+
+
 </head>
+
 <body>
+
 <div class = "nav">
 
+
+
 	  <ul>
+
 	
+
 	    <li><a href="finalhtml.php">Home</a></li>
+
 		
+
 		<li><a href="products.php">Products</a></li>
+
+
 
 		<li><a href = "shoppingCart.php">Shopping Cart</a></li>
 
+
+
 		<li><a href = "login.php">Log in</a></li>
+
+
 
 		<li><a href = "register.php">Create Account</a></li>
 
+
+
 		<li><a href = "logout.php">Logout</a></li>
+
 	
+
 	  </ul>
 
+
+
 	</div>
+
 <div class = "header1">
+
 	  <h1><i>Handcrafted Tea and Coffee</i></h1>
+
 </div>
+
 <br>
+
 <br>
+
+
 
 <?php
+
 	$servername = "localhost";
+
 	$username = "root";
+
 	$password = "";
+
 	$dbname = "beverages";
+
 	$totalCost = 0;
+
 	$id = 0;
 	
+	$Pname = $Pid = $numItems = "";
+
 	session_start();
+
 	if(isset($_SESSION["id"])){
+
 	$id = (String)$_SESSION["id"];
+
 	}
+
 	else{
+
 	echo "User isn't signed in. Unable to use shoping cart right now.";
+
 	}
+
 	
+
 	if($id !=0){
+
 		// Create connection
+
 		$conn = new mysqli($servername, $username, $password, $dbname);
+
 		// Check connection
+
 		if ($conn->connect_error) {
+
 			die("Connection failed: " . $conn->connect_error);
+
 		} 
 
-		$sql = "SELECT ProdID, totCost, numItems FROM shoppingcart WHERE id=$id";
+
+
+		$sql = "SELECT Pname, Pid, totCost, numItems FROM shoppingcart, inventory WHERE id=$id AND ProdId = Pid";
+
 		$result = $conn->query($sql);
 
+
+
 		if ($result->num_rows > 0) {
-			echo "<table><tr><th>ProdID</th><th>Cost</th><th>Number of Items</th></tr>";
+
+			echo "<table><tr><th>Item Name</th><th>Cost</th><th>Number of Items</th></tr>";
+
 			// output data of each row
+
 			while($row = $result->fetch_assoc()) {
+
 				echo "<tr>
-					<td>".$row["ProdID"]."</td>
-					<td>"."$".$row["totCost"]."</td>
-					<td>"."&nbsp".$row["numItems"]."</td>
+
+					<td>&nbsp".$row["Pname"]."</td>
+
+					<td>"."&nbsp&nbsp$".$row["totCost"]."</td>
+
+					<td>"."&nbsp&nbsp".$row["numItems"]."</td>
+
 					</tr>";
+					
+
 				$totalCost += $row["totCost"];
+
 			}
+
 			echo "</table>";
+
 		} 
+
 		else {
+
 			echo "0 results";
+
+		}
+
+		
+		if(isset($_SESSION["id"])){
+
+			$id = (String)$_SESSION["id"];
+
 		}
 		
 		if($totalCost != 0){
-			echo $totalCost;
+
+			echo "&nbspSubtotal: $" . $totalCost;
+
 		}
+
+		$actualTotal = ($totalCost*.07)+$totalCost;
+
+		print "<br>&nbspTotal Cost: $".$actualTotal;
+		
+		print '<br><br>&nbsp<input type="button" name="Checkout" value="Checkout with Saved Card" onclick="showDiv()" /><br><br>';
+
+		
+		
+		
+		//div shown after checkout button pressed. ////////////////////////////////////////////////////////////////////////////////////
+		
+		print '<div id="checkout"  style="display:none;" class="answer_list" ><br>';
+			
+				
+			print "<p>&nbspPayment of $".$actualTotal." Successful! Thank you for your purchase!</p><br>";
+			
+			$sql = "SELECT Pname, Pid, totCost, numItems FROM shoppingcart, inventory WHERE id=$id AND ProdId = Pid";
+
+			$result = $conn->query($sql);
+			
+			
+			//will use to update inventory. Also clear shopping cart?
+			$sql2 = "";
+			$result2 = "";
+			
+			if ($result->num_rows > 0) {
+
+				// output data of each row
+
+				while($row = $result->fetch_assoc()) {
+
+						
+						$Pname = $row["Pname"];
+						$Pid = $row["Pid"];
+						$numItems = $row["numItems"];
+						
+						print $Pname . " <br> ".$Pid . " <br> " . $numItems . " <br> ";
+						
+						
+						$sql2 = "UPDATE beverages.inventory SET stock =  stock - $numItems WHERE inventory.Pid = $Pid";
+						$result2 = $conn->query($sql2);
+							
+
+				}
+
+			} 
+
+	
+		print '</div>';
+		
+		
+		
+	
+
 		$conn->close();
+
 	}
+
 ?>
 
 
 
 
+	
+	<script>
+	
+	function showDiv(){
+		document.getElementById('checkout').style.display = "block";
+	}
+	</script>
+
+	
 
 </body>
+
 </html>
