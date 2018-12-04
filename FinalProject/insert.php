2075e2ab1eb736257e0cb6f0b23876375154703b
Anolting15 <42784829+Anolting15@users.noreply.github.com>
2018-12-04 11:14:30 -0500

    Used along with register.php

---
+++ FinalProject/insert.php
@@ -0,0 +1,45 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "beverages";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error){
+	die("connection failed: " .$conn->connect_error);
+}
+echo "Connected Successfully<br>";
+
+
+//Establish connection to database
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "quiz";
+
+
+if(isset($_POST['submit'])){ // Fetching variables of the form which travels in URL
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+$ccinfo = $_POST['ccinfo'];
+}
+
+
+//Insert Query of SQL
+// $sql = "INSERT INTO beverages.customer (firstname, lastname, username, password, email, ccinfo) VALUES ('$firstname', '$lastname', '$username', '$password', '$email', '$ccinfo')";
+$stmt = $conn->prepare("INSERT INTO beverages.customer(firstname, lastname, username, password, email, ccinfo) VALUES (?, ?, ?, ?, ?, ?)");
+$stmt->bind_param('ssssss', $firstname, $lastname, $username, $password, $email, $ccinfo);
+if ($firstname != null || $lastname != null || $username != null || $password != null || $email != null || $ccinfo != null){
+	$stmt->execute();
+} else print "You must enter all elements of this form";
+$stmt->close();
+
+// $query = mysql_query("insert into customer(firstname, lastname, username, password, email, ccinfo) values ('$firstname', '$lastname', '$username', '$password', '$email', '$ccinfo')");
+echo "<br/><br/><span>Data Inserted successfully...!!</span>";
+
+
+$conn->close(); // Closing Connection with Server
+?>
