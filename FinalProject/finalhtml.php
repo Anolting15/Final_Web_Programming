e5ff80dde0e5c3bd459825b5a100730f92bec0a5
cmward7400 <43648498+cmward7400@users.noreply.github.com>
2018-12-04 11:55:40 -0500

    Update finalhtml.php

--- FinalProject/finalhtml.php
+++ FinalProject/finalhtml.php
@@ -1,6 +1,6 @@
 
 <!DOCTYPE html>
-<!------>
+
 <html>
 
 <head>
@@ -15,23 +15,41 @@
 
 <body>
 
-	<div class = "nav">
+<div class = "nav">
+
+
 
 	  <ul>
+
 	
+
 	    <li><a href="finalhtml.php">Home</a></li>
+
 		
+
 		<li><a href="products.php">Products</a></li>
+
+
 
 		<li><a href = "shoppingCart.php">Shopping Cart</a></li>
 
+
+
 		<li><a href = "login.php">Log in</a></li>
+
+
 
 		<li><a href = "register.php">Create Account</a></li>
 
+
+
 		<li><a href = "logout.php">Logout</a></li>
+
 	
+
 	  </ul>
+
+
 
 	</div>
 
@@ -49,36 +67,41 @@
 	
 
 	<div class = "row">
+	
 
 	   <p><b>Sign-in or create an account to purchase one-of-a-kind, 
 
-	   handcrafted coffee or tea!</p>
+	   handcrafted coffee or tea!</b></p>
 
+	   
 	   
 
 	   <div class = "header2">
 
-	     <h1>Coffee</h1>
+	     <h1>About Us</h1>
 
 	   </div>
 
 		 	
-
-	   <p> dfjgh jsk h skjdh kasdjh gdkfgjh sdkfhg</p>
-
-			
-
+		<p>We are a local coffee shop based in Dahlonega, Georgia, hoping to enrich your life with the pleasures of hot
+		 beverages. We have a tea for every need and a coffee for every morning. Order online and we will ship you 
+		 a bag of one of the best blends of coffee or tea we could find. </p>
+		
+		
+	   
 	   <div class = "header3">
 
-	     <h1>Tea</h1>
+	     <h1>Our Products</h1>
 
 	   </div>
 
 	   
 
-	   <p>dsjgfasjdhkjasdkglhaslkdjgh hagkjas laehrgka aldhf;kglh</p>
+	    <p>We specialize in providing beverage lovers with quality tea and coffee that 
+		can't be found anywhere else. Our piping hot, strong coffees are sure to delight and perk
+		you up in a way that no other cup can. Our soothing teas gently relax and unwind even the 
+		tightest of nerves.</p>
 
-	
 
 	</div>
 
